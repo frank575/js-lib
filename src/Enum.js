0c731887fce6446893e90ad3c3c950126bcee5b8
吳俊緯 <wcw850710@gmail.com>
2021-03-07 23:08:02 +0800

    Enum: 完善類型、API 調整

--- src/Enum.js
+++ src/Enum.js
@@ -1,45 +1,33 @@
 /**
- * Enum
- * @member translation [Object] 翻譯文檔
- * @member reverseEnum [Object] 反轉的 enum
+ * @template T
+ * @template U
+ * @param {T} obj
+ * @return {T & {t(val: any): string, key(val: any): string}}
  */
-class Enum {
-	constructor(_enum, translation) {
-		this.translation = translation
-		this.reverseEnum = Object.entries(_enum).reduce(
-			(en, [name, val]) => ((en[val] = name), en),
-			{},
-		)
-	}
-	/**
-	 * 方法賦予函數
-	 * @param _enum [Object] enum
-	 * @param translation [Object] 翻譯文檔
-	 *
-	 * t (value: any): 翻譯的文字
-	 * key (value: any): [String] 傳入值取出對應的 enum key
-	 * add (key: string, value: any, t?: string): [void] 添加一個項目
-	 * delete (key: string): [void] 刪除一個項目
-	 */
-	static init(_enum, translation = {}) {
-		const obj = new Enum(_enum, translation)
+function Enum(obj) {
+	const translation = {}
+	const reverseEnum = {}
+	const $enum = {}
+	const t = val => translation[val]
+	const key = val => reverseEnum[val]
 
-		_enum.t = val => obj.translation[obj.reverseEnum[val]] ?? val
-		_enum.key = val => obj.reverseEnum[val]
-		_enum.add = (key, val, t) => {
-			_enum[key] = val
-			_enum[val] = key
-			obj.reverseEnum[val] = key
-			if (t != null) {
-				obj.translation[key] = t
-			}
-		}
-		_enum.delete = key => {
-			const val = _enum[key]
-			delete _enum[key]
-			delete obj.reverseEnum[val]
+	function addEnum(key, val) {
+		if (Array.isArray(val)) {
+			const [v, t] = val
+			translation[v] = t
+			$enum[key] = v
+			reverseEnum[v] = key
+		} else {
+			$enum[key] = val
+			reverseEnum[val] = key
 		}
 	}
+
+	for (const k in obj) {
+		addEnum(k, obj[k])
+	}
+
+	return { ...$enum, t, key }
 }
 
 export default Enum
